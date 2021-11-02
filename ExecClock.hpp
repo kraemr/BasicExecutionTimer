3024b3d7030fb161c2f85f4a4014251f8910cf0e
CodeadinC <93094049+CodeadinC@users.noreply.github.com>
2021-11-02 21:08:40 +0100

    Update ExecClock.hpp

--- ExecClock.hpp
+++ ExecClock.hpp
@@ -5,18 +5,25 @@ using namespace std;
 
 
 time_t start;
-void startexectime()
+
+time_t startexectime() //Start the Timer
 {
 time(&start);
 ios_base::sync_with_stdio(false);
+  return start;
 }
 
 time_t endtime;
-void endexectime()
+time_t endexectime(bool log) // end the timer bool 
 {
 time(&endtime);
 double timetaken = double(endtime - start);
-cout << "The piece of code took: "<<fixed<<timetaken<<setprecision(5);
-cout <<" seconds";
+  if(log)
+  {
+    cout << "The piece of code took: "<<fixed<<timetaken<<setprecision(5);
+    cout <<" seconds to complete";
+  }
+
+  return endtime;
 }
 
